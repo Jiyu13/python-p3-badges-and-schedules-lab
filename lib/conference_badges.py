e6fcc8e6b766bbb66c2337039c7ce046c18c91c6
Ziru Zou <jiyucoding@yahoo.com>
2023-02-19 02:47:08 -0500

    complete lab

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,18 @@
+# 1. return a message with name
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
 
+#  2. takes a list of names as an argument and returns a list of badge messages
 def batch_badge_creator(names):
-    return None
+    return [f"Hello, my name is {name}." for name in names]
 
+#  3. takes the list of speakers and assigns each speaker to a room
 def assign_rooms(names):
-    return None
+    return [f"Hello, {name}! You'll be assigned to room {names.index(name) + 1}!" for name in names]
 
+#  4. 
 def printer(names):
-    return None
+    for badge in batch_badge_creator(names):
+        print(badge)
+    for assignment in assign_rooms(names):
+        print(assignment)
